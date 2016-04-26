e5c43d82ec4febe45dec7a2df3bdbe3cd410c252
Vicente Martínez <vicentemartinezf@gmail.com>
2016-04-26 23:17:21 +0200

    Prototipo de web

--- build/web/js/banner.js
+++ build/web/js/banner.js
@@ -7,7 +7,7 @@
 var array_imagen;
 array_imagen = new Array(4) ;
 array_imagen[0] = new Image(120,41) ;
-array_imagen[0].src = "Imagenes/img1.jpg" ;
+array_imagen[0].src = "Imagenes/granja.jpg" ;
 array_imagen[1] = new Image(120,41) ;
 array_imagen[1].src = "Imagenes/img2.jpg" ;
 array_imagen[2] = new Image(120,41) ;
@@ -15,12 +15,35 @@ array_imagen[2].src = "Imagenes/img3.png";
 array_imagen[3] = new Image(120,41) ;
 array_imagen[3].src = "Imagenes/img4.jpg";
 
+var array_url;
+array_url = new Array(4) ;
+array_url[0] = "Noticias/noticia-basta-maltrato-granjas.html";
+array_url[1] = "Imagenes/img2.jpg" ;
+array_url[2]= "Imagenes/img3.png"; 
+array_url[3]= "Imagenes/img4.jpg";
+
+var array_tit;
+array_tit = new Array(4) ;
+array_tit[0] = "Basta de maltrato en las granjas de animales";
+array_tit[1] = "Basta de maltrato en las granjas de animales" ;
+array_tit[2]= "Tercer titulo"; 
+array_tit[3]= "Cuarto titulo";
+
+var array_tex;
+array_tex = new Array(4) ;
+array_tex[0] = "Si alguien le hiciera a un perro o a un gato lo que la ganadería industrial hace a los animales de granja, acabaría en la cárcel. La diferencia es que los animales víctimas de la ganadería industrial son cerdos, vacas, terneras, pollos y gallinas, entre  otros.  ";
+array_tex[1] = "Basta de maltrato en las granjas de animales" ;
+array_tex[2]= "Parrafo con las primeras lineas de la noticia correspondiente"; 
+array_tex[3]= "Parrafo con las primeras lineas de la noticia correspondiente";
 //variable para llevar la cuenta de la imagen siguiente 
 contador = 0 ;
 var time;
 //función para rotar el banner 
 function alternar_banner(){ 
    	document.getElementById("centro").src = array_imagen[contador].src ;
+        document.getElementById("link").href = array_url[contador];
+        document.getElementById("titulo").textContent = array_tit[contador];
+        document.getElementById("texto").textContent = array_tex[contador];
    	contador ++ ;
    	if(contador>3){contador=0;} 
    	time=setTimeout("alternar_banner()",10000) ;
@@ -31,6 +54,9 @@ function izquierda(){
     if(contador===-1) contador=3;
     if(contador===-2) contador=2;
     document.getElementById("centro").src = array_imagen[contador].src ;
+    document.getElementById("link").href = array_url[contador];
+    document.getElementById("titulo").textContent = array_tit[contador];
+    document.getElementById("texto").textContent = array_tex[contador];
     contador++;
     clearTimeout(time);
     time=setTimeout("alternar_banner()",10000) ;
@@ -39,6 +65,9 @@ function izquierda(){
 function derecha(){
     if(contador>3) contador=0;
     document.getElementById("centro").src = array_imagen[contador].src ;
+    document.getElementById("link").href = array_url[contador];
+    document.getElementById("titulo").textContent= array_tit[contador];
+    document.getElementById("texto").textContent = array_tex[contador];
     contador++;
     clearTimeout(time);
     time=setTimeout("alternar_banner()",10000) ;
